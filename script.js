a9d67d0f5bc205301516614653a79359aefc3f82
EReaX <67808914+EReaXe@users.noreply.github.com>
2023-11-05 00:01:58 +0300

    5 November 2023

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let leaderboardData = [
   { name: "Ali K.", score: 3 },
   { name: "Beof Karaca", score: 1 },
-  { name: "Erkan Erkanson", score: 3 },
+  { name: "Erkan Erkanson", score: 3.5 },
   { name: "Etli Pilav", score: 0.5 },
   { name: "Faruk", score: 3.5 },
   { name: "G. Ahmet M. P.", score: 4 },
