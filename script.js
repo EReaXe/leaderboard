bf76cd4c656a1f81a532862dae2c747875beb9ed
EReaX <67808914+EReaXe@users.noreply.github.com>
2023-11-26 01:38:39 +0300

    26 November 2023

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Sample leaderboard data
 let leaderboardData = [
   { name: "Ali K.", score: 14 },
-  { name: "Beof Karaca", score: 25 },  
+  { name: "Beof Karaca", score: 24 },  
   { name: "Erkan Erkanson", score: 12 },
-  { name: "Etli Pilav", score: 26 },
+  { name: "Etli Pilav", score: 25 },
   { name: "Faruk", score: 13 },
-  { name: "G. Ahmet M. P.", score: 22 },
-  { name: "LunarLords", score: 23 },
+  { name: "G. Ahmet M. P.", score: 21 },
+  { name: "LunarLords", score: 22 },
   { name: "Melih Dolarım", score: 17 },
-  { name: "Minar Sinan", score: 24 },
-  { name: "Mustafa Rock", score: 21 },
+  { name: "Minar Sinan", score: 23 },
+  { name: "Mustafa Rock", score: 20 },
 ];
 
 // Function to populate the leaderboard table
